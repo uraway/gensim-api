8f287cfbc8c3a225a5a0eb64b48b95f5c98d4861
uraway <masato.u0107@gmail.com>
2016-09-11 18:15:01 +0800

    improve server

--- server.py
+++ server.py
@@ -16,16 +16,9 @@ class Server(object):
     def __init__(self):
         conn = boto.connect_s3(AWS_ACCESS_ID, AWS_ACCESS_TOKEN)
         bucket = conn.get_bucket('gensim-models')
-        bucket_list = bucket.list()
-        if not bucket_list:
-            print 'Model could not be downloaded from S3'
-        else:
-            print bucket_list
 
-        for l in bucket_list:
-            keyString = str(l.key)
-            if not os.path.exists(LOCAL_PATH + keyString):
-                l.get_contents_to_filename(LOCAL_PATH + keyString)
+        key = bucket.get_key('jawiki_wakati.model')
+        key.get_contents_to_filename('./models/jawiki_wakati.model')
 
         self.logger = logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
         self.model = word2vec.Word2Vec.load("./models/jawiki_wakati.model")
