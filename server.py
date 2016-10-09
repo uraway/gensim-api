2675a076b58ec44031c74ae5e4105a9c9cd70787
uraway <masato.u0107@gmail.com>
2016-10-09 20:40:11 +0900

    improve server.py

--- server.py
+++ server.py
@@ -4,22 +4,10 @@ import falcon
 from gensim.models import word2vec
 import logging
 import os, sys
-import boto
-import boto.s3.connection
-
-LOCAL_PATH='./models/'
-AWS_ACCESS_ID=os.getenv('AWS_ACCESS_ID')
-AWS_ACCESS_TOKEN=os.getenv('AWS_ACCESS_TOKEN')
 
 class Server(object):
 
     def __init__(self):
-        conn = boto.connect_s3(AWS_ACCESS_ID, AWS_ACCESS_TOKEN)
-        bucket = conn.get_bucket('gensim-models')
-
-        key = bucket.get_key('jawiki_wakati.model')
-        key.get_contents_to_filename('./models/jawiki_wakati.model')
-
         self.logger = logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
         self.model = word2vec.Word2Vec.load("./models/jawiki_wakati.model")
 
