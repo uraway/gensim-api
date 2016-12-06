f230e84be0362ce62923895de3fc2694e84ee46d
uraway <masato.u0107@gmail.com>
2016-12-06 18:38:44 +0900

    handling error

--- server.py
+++ server.py
@@ -16,8 +16,13 @@ class Server(object):
         s1 = req.get_param('s1')
         s2 = req.get_param('s2')
 
+        try:
+            sim = self.model.similarity(s1, s2)
+        except:
+            sim = 0
+
         content = {
-            "similarity": self.model.similarity(s1, s2)
+            "similarity": sim
         }
 
         res.body = json.dumps(content)
