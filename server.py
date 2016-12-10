4edc21eec6d0240d9e28fc636057282595885df4
uraway <masato.u0107@gmail.com>
2016-12-10 15:11:50 +0900

    v2

--- server.py
+++ server.py
@@ -12,20 +12,32 @@ class Server(object):
         self.model = word2vec.Word2Vec.load("./models/jawiki_wakati.model")
 
     def on_get(self, req, res):
-
+        s = req.get_param('s')
         s1 = req.get_param('s1')
         s2 = req.get_param('s2')
 
-        try:
-            sim = self.model.similarity(s1, s2)
-        except:
-            sim = 0
+        if s1 and s2:
+            try:
+                sim = self.model.similarity(s1, s2)
+            except:
+                sim = 0
 
-        content = {
-            "similarity": sim
-        }
+            content = {
+                "similarity": sim
+            }
+            res.body = json.dumps(content)
 
-        res.body = json.dumps(content)
+        else:
+            try:
+                result = self.model.most_similar(positive = s, negative = [], topn = 3)
+            except:
+                result = []
+
+            for i in result:
+                print i[0]
+
+            res.body = json.dumps(result)
+
 
 app = falcon.API()
 app.add_route("/", Server())
